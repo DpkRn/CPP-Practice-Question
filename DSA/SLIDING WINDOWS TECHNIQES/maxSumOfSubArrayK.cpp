55aab7fcce61c0d9f181165e4ff88e56d68b75b5
DpkRn <dwizardtechno@gmail.com>
2023-07-06 22:10:33 +0530

    another techniqe of maxSumofArrayK

--- DSA/SLIDING WINDOWS TECHNIQES/maxSumOfSubArrayK.cpp
+++ DSA/SLIDING WINDOWS TECHNIQES/maxSumOfSubArrayK.cpp
@@ -1,18 +1,36 @@
  #include<bits/stdc++.h>
  using namespace std;
+// int maxSum(int arr[],int n,int k){
+//     int max_sum=0;
+//     for(int i=0;i<k;i++){
+//         max_sum+=arr[i];
+//     }
+//     int win_sum=max_sum;
+//     int i=k;
+// while(i<n){
+// win_sum=win_sum+arr[i]-arr[i-k];
+// max_sum=max(max_sum,win_sum);
+// i++;
+// }
+// return max_sum;
+// }
+
 int maxSum(int arr[],int n,int k){
-    int max_sum=0;
-    for(int i=0;i<k;i++){
-        max_sum+=arr[i];
-    }
-    int win_sum=max_sum;
-    int i=k;
-while(i<n){
-win_sum=win_sum+arr[i]-arr[i-k];
-max_sum=max(max_sum,win_sum);
-i++;
+int i=0,j=0,maxv=INT_MIN,sum=0;
+while(j<n){
+    sum=sum+arr[j];
+if(j-i+1<k){
+  j++;
 }
-return max_sum;
+else
+{
+    maxv=max(sum,maxv);
+    j++;
+    sum=sum-arr[i];
+    i++;
+}
+}
+return maxv;
 }
  int main()
 {
